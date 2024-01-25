ab9a19f978b3c800b4d671cbe7b1013e2c613539
Abhishekranjancodes <99069530+Abhishekranjancodes@users.noreply.github.com>
2024-01-26 02:23:22 +0530

    Capture images for calibration using this code

---
+++ Resources/Resources/capture_image.py
@@ -0,0 +1,68 @@
+import cv2 as cv
+import os
+
+Chess_Board_Dimensions = (8, 5)
+
+n = 0  # image counter
+
+# checks images dir is exist or not
+image_path = "images"
+
+Dir_Check = os.path.isdir(image_path)
+
+if not Dir_Check:  
+    os.makedirs(image_path)
+    print(f'"{image_path}" Directory is created')
+else:
+    print(f'"{image_path}" Directory already exists.')
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+
+def detect_checker_board(image, grayImage, criteria, boardDimension):
+    ret, corners = cv.findChessboardCorners(grayImage, boardDimension)
+    if ret == True:
+        corners1 = cv.cornerSubPix(grayImage, corners, (3, 3), (-1, -1), criteria)
+        image = cv.drawChessboardCorners(image, boardDimension, corners1, ret)
+
+    return image, ret
+
+
+cap = cv.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    copyFrame = frame.copy()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    image, board_detected = detect_checker_board(
+        frame, gray, criteria, Chess_Board_Dimensions
+    )
+    cv.putText(
+        frame,
+        f"saved_img : {n}",
+        (30, 40),
+        cv.FONT_HERSHEY_PLAIN,
+        1.4,
+        (0, 255, 0),
+        2,
+        cv.LINE_AA,
+    )
+
+    cv.imshow("frame", frame)
+    cv.imshow("copyFrame", copyFrame)
+
+    key = cv.waitKey(1)
+
+    if key == ord("q"):
+        break
+    if key == ord("s") and board_detected == True:
+        
+        cv.imwrite(f"{image_path}/image{n}.png", copyFrame)
+
+        print(f"saved image number {n}")
+        n += 1  
+cap.release()
+cv.destroyAllWindows()
+
+print("Total saved Images:", n)
